9d9d432b271e07c28530b8d2d65a4f2ec92c402e
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2020-03-09 11:15:48 +0100

    Add My Reservations + modifications.

---
+++ app/src/main/java/com/example/berclazmay_skiseller/ui/myreservations/MyReservationsFragment.java
@@ -0,0 +1,26 @@
+package com.example.berclazmay_skiseller.ui.myreservations;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.berclazmay_skiseller.R;
+import com.example.berclazmay_skiseller.ui.home.HomeViewModel;
+
+public class MyReservationsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_myreservations, container, false);
+        return root;
+    }
+}
